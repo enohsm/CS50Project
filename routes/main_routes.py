915de3067fd9b2830ec43f0cdaff06753c93b608
enohsm <mustafaenes1998@gmail.com>
2025-10-15 17:58:10 +0300

    The login function has been implemented.

--- routes/main_routes.py
+++ routes/main_routes.py
@@ -5,6 +5,10 @@ from .imports import *
 # TR: "main" rotalarını modülleyecek bir değişken ata
 mainbp = Blueprint("main", __name__)
 
+# ENG: Assign variable to read the data from database
+# TR: Veritabanından okunan veriyi bir değişkene atıyorum
+db = SQL("sqlite:///database.db")
+
 # ENG: Some user operations are handled within the "main" routes.
 # TR: "main" rotaları kullanıcı işlemlerinin de bir kısmını kapsar.
 
@@ -18,9 +22,34 @@ def register():
     return render_template("register.html")
 
 
-@mainbp.route("/login")
+@mainbp.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        if not username:
+            flash("Username cannot be empty.")
+            return redirect("/login")
+
+        password = request.form.get("password")
+        if not password:
+            flash("Password cannot be empty.")
+            return redirect("/login")
+        
+        user = db.execute("SELECT id, username, password FROM users WHERE username = ?", username)
+        if not user:
+            flash("Invalid username.")
+            return redirect("/login")
+        
+        if password != user[0]["password"]:
+            flash("Invalid password.")
+            return redirect("/login")
+        
+        session["user_id"] = user[0]["id"]
+
+        return("/")
+        
+    else:
+        return render_template("login.html")
 
 
 @mainbp.route("/logout")
