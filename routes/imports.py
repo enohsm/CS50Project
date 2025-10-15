647ea4573a50b67aa6e3487188407a16df3d90bd
enohsm <mustafaenes1998@gmail.com>
2025-10-15 17:58:10 +0300

    The login function has been implemented.

--- routes/imports.py
+++ routes/imports.py
@@ -1,2 +1,3 @@
-from flask import Blueprint, render_template, request, redirect, session
+from flask import Blueprint, render_template, request, redirect, session, flash, url_for
 from flask_session import Session
+from cs50 import SQL
